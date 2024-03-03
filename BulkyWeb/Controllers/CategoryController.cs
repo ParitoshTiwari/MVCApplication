fc6671a9aafdc4eba7d8d1b155be20f4bd23cf64
Paritosh Tiwari <Paritosh1.tiwari@gmail.com>
2024-03-03 13:21:46 -0600

    Adding toastr partial Layout page Edit/Delete Items

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -34,9 +34,69 @@ namespace BulkyWeb.Controllers
             {
                 _context.categories.Add(category);
                 _context.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _context.categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.categories.Update(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _context.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _context.categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _context.categories.Remove(obj);
+            _context.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
         }
     }
 }
