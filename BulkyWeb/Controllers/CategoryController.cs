aea237deb66fa319e9d1e7902da2349eceac45d3
Paritosh Tiwari <Paritosh1.tiwari@gmail.com>
2024-03-03 10:47:45 -0600

    Changes to add Initial Project Add Migrations Add Db Context

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _context.categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
